472687271f5a57a07e80442ffdfb0a8138e4eec8
Mihir160 <mihir.roy266@gmail.com>
2022-11-26 12:29:53 +0600

    admin api

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ async function run() {
         const productsCollection = client.db('used-products-resale').collection('products');
         const usersCollection = client.db('used-products-resale').collection('users');
         const bookingsCollection = client.db('used-products-resale').collection('bookings');
+        const paymentsCollection = client.db('used-products-resale').collection('payments');
 
         app.get('/categories', async (req, res) => {
             const query = {};
@@ -50,11 +51,43 @@ async function run() {
             const result = await usersCollection.insertOne(user)
             res.send(result)
         })
+        app.get('/users', async(req, res) =>{
+            const role = req.query.role
+            const query = { role: role };
+            const seller = await usersCollection.find(query).toArray();
+            res.send(seller)
+        })
+
+        app.delete('/users/:id', async (req, res) =>{
+            const id = req.params.id;
+            const filter = {_id : Object(id)}
+            const result = await usersCollection.deleteOne(filter)
+            res.send(result)
+        })
+
+        app.get('/users/admin/:email', async(req, res) =>{
+            const email = req.params.email;
+            const query = { email }
+            const user = await usersCollection.findOne(query)
+            res.send({isAdmin: user?.role === 'admin'})
+        })
 
         app.post('/bookings', async(req, res) =>{
             const user = req.body
             const result = await bookingsCollection.insertOne(user)
             res.send(result)
+        })
+        app.get('/bookings',  async (req, res) => {
+            const email = req.query.email;
+            // const decodedEmail = req.decoded.email;
+
+            // if (email !== decodedEmail) {
+            //     return res.status(403).send({ message: 'forbidden access' });
+            // }
+
+            const query = { email: email };
+            const bookings = await bookingsCollection.find(query).toArray();
+            res.send(bookings);
         })
     }
     finally {
